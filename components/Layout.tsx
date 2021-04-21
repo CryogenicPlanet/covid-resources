058667f96a2452adbc5f07a894dcd7036cdeab25
Siddhant Khare <Siddhantkhare2694@gmail.com>
2021-04-21 18:56:51 +0530

    Updated GitHub Link of repo.

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -23,7 +23,7 @@ const navigation = [
   },
   {
     name: 'GitHub',
-    href: 'https://github.com/CryogenicPlanet/covid-bangalore-resources',
+    href: 'https://github.com/CryogenicPlanet/covid-resources',
     icon: (props: any) => (
       <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
         <path
